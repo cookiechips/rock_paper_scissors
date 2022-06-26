c296c415a5392e33aec9299f57f1b64ddde72bc9
CookieChips <cookiechips@gmail.com>
2022-06-26 22:21:36 +0300

    Change the def

--- number_guessing.py
+++ number_guessing.py
@@ -2,33 +2,38 @@ import random
 from unicodedata import numeric
 
 random_num = random.randint(1,50)
-number = int(input("Guess the Number from 1 to 50!!!!\n"))
-list_of_guess = []
+attempts = []
+guess = int(input("Pick a number between 1 and 50\n"))
 
-def check_num(number):
+
+
+def guess_check(guess):
     while True:
-        if number > 50:
-            number = int(input('You are out of limits. Guess again\n'))
-        else:
-            break
-    return number
+        try:
+            if guess > 50:
+                guess = int(input('You are out of limits. Pick a number between 1 and 50\n'))
+            else:
+                break   
+        except ValueError as err:
+            print("Oh no!, that is not a valid value. Please type a numeric input")
+            print("({})".format(err))
+    return guess
 
 
- 
 while True:
-    try:
-        number_checked = check_num(number)
 
-        if number_checked > random_num:
-            list_of_guess.append(number_checked)
-            number = int(input('You are high. Go Lower!\n '))
-        elif number_checked < random_num:
-            list_of_guess.append(number_checked)
-            number = int(input('You are low. Go Higher!\n '))
-        elif number == random_num:
-            list_of_guess.append(number_checked)
-            print(f'Congratulations you guessed the right number on your {len(list_of_guess)}th try!')
-            print(list_of_guess)
-            break
-    except ValueError:
-        number = int(input("That's not a number. Give a numeric input\n"))
+    checked_guess = guess_check(guess)
+    
+    if checked_guess > random_num:
+        attempts.append(checked_guess)
+        print('Go Lower!')
+        guess = int(input("Pick again\n"))
+    elif checked_guess < random_num:
+        attempts.append(checked_guess)
+        print('Go Higher!')
+        guess = int(input("Pick again\n"))
+    elif checked_guess == random_num:
+        attempts.append(checked_guess)
+        print(f'Congratulations you guessed the right number on your {len(attempts)}th try!')
+        print(attempts)
+        break
